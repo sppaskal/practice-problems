2a96bfce8584d2a61779d293d693a9f6c5147898
sppaskal <paskalevslavi@gmail.com>
2025-08-04 11:03:06 -0600

    Adding solution to "flatten nested list" problem

---
+++ python/easy/flatten_nested_list/main.py
@@ -0,0 +1,23 @@
+'''
+Use a recursive approach to traverse
+all the nested lists within the main 
+list. Use .extend() to add all elems
+of each item (when it's an iterable)
+to the flat list, or simply append when
+the item is not an iterable.
+'''
+
+
+def flatten_nested_list(nested_list):
+    flat = []
+
+    for item in nested_list:
+        if isinstance(item, list):
+            flat.extend(flatten_nested_list(item))
+        else:
+            flat.append(item)
+
+    return flat
+
+
+print(flatten_nested_list([1, [2, [3, 4], 5], 6]))
