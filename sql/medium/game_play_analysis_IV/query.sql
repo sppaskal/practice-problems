a81221d9b31cf660312fac300ec83bda599ff4d0
sppaskal <sppaskal@users.noreply.github.com>
2025-11-30 16:40:14 -0800

    Adding PostgreSQL solution for 'game play analysis IV' problem

--- sql/medium/game_play_analysis_IV/query.sql
+++ sql/medium/game_play_analysis_IV/query.sql
@@ -37,3 +37,35 @@ FROM
     LoggedInAgain l
 CROSS JOIN 
     TotalPlayers t;
+
+-- PostgreSQL
+
+WITH FirstLogin AS (
+    SELECT
+        player_id,
+        MIN(event_date) AS first_login_date
+    FROM Activity
+    GROUP BY player_id
+),
+ConsecutiveLogins AS (
+    SELECT a.player_id
+    FROM Activity a
+    JOIN FirstLogin f1
+        ON a.player_id = f1.player_id
+        -- PostgreSQL syntax for adding 1 day
+        AND a.event_date = f1.first_login_date + INTERVAL '1 day'
+),
+TotalPlayers AS (
+    SELECT COUNT(DISTINCT player_id) AS total_players
+    FROM Activity
+),
+LoggedInAgain AS (
+    SELECT COUNT(DISTINCT player_id) AS logged_in_again
+    FROM ConsecutiveLogins
+)
+SELECT ROUND(
+    1.0 * logged_in_again / total_players,  -- force float division
+    2
+) AS fraction
+FROM LoggedInAgain
+CROSS JOIN TotalPlayers;
